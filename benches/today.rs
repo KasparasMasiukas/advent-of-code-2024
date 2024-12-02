f2b43ac1571e4f4b545a7197ed6d02f085e504e5
Kasparas Masiukas <kasparas.masiukas@addepar.com>
2024-12-02 19:52:34 +0000

    Change "today" to Day 2

--- benches/today.rs
+++ benches/today.rs
@@ -2,7 +2,7 @@ use advent_of_code_2024::day1::{part1, part2};
 use criterion::{criterion_group, criterion_main, Criterion};
 mod common;
 
-const DAY: u32 = 1;
+const DAY: u32 = 2;
 
 fn run_benchmarks(c: &mut Criterion) {
     common::run_benchmarks(c, DAY, part1, part2);
