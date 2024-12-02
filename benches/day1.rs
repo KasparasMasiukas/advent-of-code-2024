21b5e3bf3a5c189d80868b5d5e8ba5a7cc3b686a
Kasparas Masiukas <kasparas.masiukas@addepar.com>
2024-12-02 20:04:19 +0000

    Fix benches

--- benches/day1.rs
+++ benches/day1.rs
@@ -4,8 +4,16 @@ mod common;
 
 const DAY: u32 = 1;
 
+fn part1_boxed(input: &str) -> Box<dyn std::fmt::Display> {
+    Box::new(part1(input))
+}
+
+fn part2_boxed(input: &str) -> Box<dyn std::fmt::Display> {
+    Box::new(part2(input))
+}
+
 fn run_benchmarks(c: &mut Criterion) {
-    common::run_benchmarks(c, DAY, part1, part2);
+    common::run_benchmarks(c, DAY, part1_boxed, part2_boxed);
 }
 
 criterion_group!(benches, run_benchmarks);
